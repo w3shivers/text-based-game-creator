0dd12fbf909aec0cd2766d807aa788e209b49ac6
Rudi Esterhuysen <rudi.esterhuyzen@gmail.com>
2023-06-12 21:04:17 +0200

    Fixed the char bug for color based ascii art.

--- image.py
+++ image.py
@@ -4,19 +4,15 @@ from rich import print
 
 class AsciiArt():
     ascii_characters: str = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-    __is_new_line: bool
 
     def create(self) -> None:
-        image = Image.open('image.jpg')
+        image = Image.open('cat.jpg')
         # image = image.resize((width, height))
-        image = image.resize((70, 35))
+        image = image.resize((90, 35))
         ascii_art = self.convert_to_ascii_art(image=image)
         self.print_lines(ascii_art=ascii_art)
 
     def convert_to_ascii_art(self, image: Image) -> list:
-        """ Still need to escape some chars here. It is 
-        not terrible, but the ascii art is out of line
-        due to some color wrappers causing issues. """
         previous_color = ''
         ascii_art = []
         (width, height) = image.size
@@ -25,6 +21,8 @@ class AsciiArt():
             for x in range(0, width - 1):
                 pixel = image.getpixel((x, y))
                 character = self.convert_pixel_to_character(pixel=pixel)
+                if character == '\\':
+                    character = '\\\\' # Have to double escape the backslash to ensure it doesn't escape any color tags. 
                 hex_color = rgb_to_hex(pixel)
                 if not line:
                     line += f'[{hex_color}]{character}'
@@ -32,8 +30,10 @@ class AsciiArt():
                     line += f'[/][{hex_color}]{character}'
                 else:
                     line += character
+                # line += character
                 previous_color = hex_color
             ascii_art.append(line + '[/]')
+            # ascii_art.append(line)
         return ascii_art
 
     def convert_pixel_to_character(self, pixel: tuple) -> str:
